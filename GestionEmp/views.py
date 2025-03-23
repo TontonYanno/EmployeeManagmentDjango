f0930c4a9071d1a2294e76a2a750e1b12f1982bb
yann codeur <yanncodeur1@gmail.com>
2025-03-23 02:26:40 +0100

    intergrattion des vue template ok

--- GestionEmp/views.py
+++ GestionEmp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'admin/gestionuser/edituser.html')
+    return render(request, 'emp/conges/congesdetails.html')
