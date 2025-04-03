4335ea01027cbb2b076f56a2f678162b133dfbd1
yann codeur <yanncodeur1@gmail.com>
2025-04-03 19:35:43 +0200

    toute la gestion user est ok il reste les envoie de mail niveau admin

--- User/urls.py
+++ User/urls.py
@@ -4,13 +4,16 @@ from django.urls import path
 urlpatterns = [
     #Les routes pour l'employé
     path('myprofile', views.myprofile, name='myprofile'),
-    
     path('changepassword/', views.editprofile, name='editprofile'),
-    path('change_password/', views.change_password, name='change_password'),
+    path('change_password/', views.change_password, name='change_password'), #le traitement du changement de mot de passe
     
     #Les routes pour l'admin
     path('adduser/', views.users, name='addusers'),
+    path('add_user/', views.register, name='add_user'), #le traitement de l'ajout d'un utilisateur
     path('listusers/', views.listusers, name='listusers'),
-    path('edituser/', views.edituser, name='edituser'),
+    path('edituser/<int:id>/', views.edituser, name='edituser'), #affichage
+    path('update_user/<int:id>/', views.update_user, name='update_user'), #le traitement de la modification d'un utilisateur
+    path('delete_user/<int:id>/', views.delete_user, name='delete_user'), #le traitement de la suppression d'un utilisateur
     
+  
 ]
--- User/views.py
+++ User/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import update_session_auth_hash
-from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import is_password_usable
 from django.contrib import messages
-from django.contrib.auth.hashers import check_password
-from django.contrib.auth import authenticate , login , logout
-
+from django.contrib.auth.hashers import check_password, make_password
+from User.models import Utilisateur
 
 
 # Create your views here.
@@ -21,8 +19,6 @@ def editprofile(request):
     user= request.user
     return render(request, 'emp/profile/editprofile.html', {"user":user})
 
-
-# @login_required
 def change_password(request):
     if request.method == "POST":
         
@@ -52,11 +48,109 @@ def change_password(request):
 
 
 #Les Vues Admin
+@login_required
 def users(request):
+    user= request.user
+    return render(request, 'admin/gestionuser/adduser.html',{"user":user})
+
+def register(request):
+    if request.method == 'POST':
+        #Traitemant pour automatiser le matricule
+        def generate_matricule():
+            prefix = "EMP"
+            numero = Utilisateur.objects.count() + 1
+            matricule = f"{prefix}{numero:03d}"
+            return matricule
+        
+        username = request.POST.get('username')
+        nom_complet = request.POST.get('nom_complet')
+        email = request.POST.get('email')
+        sexe = request.POST.get('sexe')
+        matricule = generate_matricule()
+        telephone = request.POST.get('telephone')
+        adresse = request.POST.get('adresse')
+        date_naissance = request.POST.get('date_naissance')
+        role = request.POST.get('role')
+        password = request.POST.get('password')
+      
+        if Utilisateur.objects.filter(email=email).exists():
+            messages.error(request, "Cet email est déjà utilisé.")
+            return redirect("addusers")
+        if Utilisateur.objects.filter(telephone=telephone).exists():
+            messages.error(request, "Ce numéro de téléphone est déjà utilisé.")
+            return redirect("addusers")
+        
+        # Enregistrement de l'utilisateur
+        user = Utilisateur.objects.create_user(
+            username=username,
+            email=email,
+            nom_complet=nom_complet,
+            sexe=sexe,
+            telephone=telephone,
+            adresse=adresse,
+            date_naissance=date_naissance,
+            role=role,
+            password=password,
+            matricule=matricule,
+        )
+        user.save()
+        # messages.success(request, "Utilisateur ajouté avec succès !")
+        return redirect('listusers')  # Redirige vers la liste des utilisateurs
+    
     return render(request, 'admin/gestionuser/adduser.html')
 
-def listusers(request):
-    return render (request, 'admin/gestionuser/listuser.html')
 
-def edituser(request):
-    return render(request, 'admin/gestionuser/edituser.html')
+def update_user(request, id):
+    user = get_object_or_404(Utilisateur, id=id)
+    if request.method == 'POST':
+        if user.password != request.POST.get('password'): # Vérifie si le mot de passe a été modifié
+            # Si le mot de passe a été modifié, on le hache avant de l'enregistrer
+            user.username = request.POST.get('username')
+            user.nom_complet = request.POST.get('nom_complet')
+            user.email = request.POST.get('email')
+            user.sexe = request.POST.get('sexe')
+            user.telephone = request.POST.get('telephone')
+            user.adresse = request.POST.get('adresse')
+            user.date_naissance = request.POST.get('date_naissance')
+            user.role = request.POST.get('role')
+            user.password = request.POST.get('password')
+            # Hachage du mot de passe avant de l'enregistrer
+            user.password=make_password(user.password)
+            
+            # Enregistrement des modifications
+            user.save()
+            messages.success(request, "Utilisateur modifié avec succès !")
+            return redirect('listusers')  # Redirige vers la liste des utilisateurs
+            
+        else:
+            # Si le mot de passe n'a pas été modifié, on ne le change pas
+            user.username = request.POST.get('username')
+            user.nom_complet = request.POST.get('nom_complet')
+            user.email = request.POST.get('email')
+            user.sexe = request.POST.get('sexe')
+            user.telephone = request.POST.get('telephone')
+            user.adresse = request.POST.get('adresse')
+            user.date_naissance = request.POST.get('date_naissance')
+            user.role = request.POST.get('role')
+            # Enregistrement des modifications
+            user.save()
+            messages.success(request, "Utilisateur modifié avec succès !")
+            return redirect('listusers')  # Redirige vers la liste des utilisateurs
+    return render(request, 'admin/gestionuser/edituser.html', {"user":user})
+
+def delete_user(request, id):
+    user = get_object_or_404(Utilisateur, id=id)
+    
+    user.delete()
+    messages.success(request, "Utilisateur supprimé avec succès !")
+    return redirect('listusers')  # Redirige vers la liste des utilisateurs
+
+@login_required
+def listusers(request):
+    user= request.user
+    users = Utilisateur.objects.all()
+    return render (request, 'admin/gestionuser/listuser.html',{"user":user,"users":users})
+
+def edituser(request,id):
+    user = get_object_or_404(Utilisateur, id=id)
+    return render(request, 'admin/gestionuser/edituser.html', {"user":user})
